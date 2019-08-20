c23f5fbfe1901c2f87d7d7f6843d8f9a4bcc2858
LAPTOP-AC8ASB69\jagut <jagut.jeremy@gmail.com>
2019-08-20 21:19:31 +0200

    Configuration de Firebase + route de connexion

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'G2Resto';
+  constructor(){
+    //Firebase configuration
+    var firebaseConfig = {
+      apiKey: "AIzaSyAbmHC0gyaFCPYawSv91B8-Vf9-_5ahfb0",
+      authDomain: "g2resto-2e83d.firebaseapp.com",
+      databaseURL: "https://g2resto-2e83d.firebaseio.com",
+      projectId: "g2resto-2e83d",
+      storageBucket: "",
+      messagingSenderId: "385978229022",
+      appId: "1:385978229022:web:58288b2c5b1c1385"
+    };
+    //Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { SigninComponent } from './signin/signin.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    SigninComponent,
   ],
   imports: [
     BrowserModule,
