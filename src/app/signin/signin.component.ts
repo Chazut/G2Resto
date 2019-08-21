67ad53d01d0f8a099e2c474c70e9b556fb217387
LAPTOP-AC8ASB69\jagut <jagut.jeremy@gmail.com>
2019-08-21 19:21:47 +0200

    Formulaire de connexion grâce au module ReactiveForms d'angular

--- src/app/signin/signin.component.ts
+++ src/app/signin/signin.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm, FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-signin',
@@ -7,9 +8,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SigninComponent implements OnInit {
 
-  constructor() { }
+  signinForm: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit() {
+    this.initForm();
+  }
+
+  initForm(){
+    this.signinForm = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.pattern(/[0-9a-zA-Z]{6,}/)]]
+    });
+  }
+
+  onAuth(){
+    const email = this.signinForm.get('email').value;
+    const password = this.signinForm.get('password').value;
+    console.log(email + '  ' + password);
   }
 
 }
